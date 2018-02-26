d8f3988d6657d28efdca8275f5a27e762a2c77ba
WeedDominator <bero4net@gmail.com>
2018-02-26 19:47:18 +0100

    Fixed click bug between buttons

--- js/app.js
+++ js/app.js
@@ -3,19 +3,21 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function  menuClickHandler(element){
-	switch (element.path[1].id) {
-		case "classic":
-			break;
-		case "classic-time":
-			break;
-		case "classic-shuffle":
-			break;
-		case "memorize":
-			break;
-		case "memorize-shuffle":
-			break;
-		default:
-			console.log("Error unknown button pressed");
-			console.log(element);
+	if(element.path[1].id != "main-menu"){
+		switch (element.path[1].id) {
+			case "classic":
+				break;
+			case "classic-time":
+				break;
+			case "classic-shuffle":
+				break;
+			case "memorize":
+				break;
+			case "memorize-shuffle":
+				break;
+			default:
+				console.log("Error unknown button pressed");
+				console.log(element);
+		}
 	}
 }
