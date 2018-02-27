9a575be2cd01c5999135e194f32378e2374fa87a
WeedDominator <bero4net@gmail.com>
2018-02-27 17:03:34 +0100

    Add icon list

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+/* Cards!
+List of icon used for Cards
+
+user-secret		tint	ambulance		graduation-cap
+shopping-bag	bug 	bomb			bell
+birthday-cake	car		camera			fire-extinguisher
+camera-retro 	bus		briefcase		football-ball
+bowling-ball 	dna 	compass			thermometer-half
+envelope 		eye		female			heartbeat image
+eye-dropper		fly		filter			volume-up
+first-aid		gem		gamepad
+fire flag		bath	headphones
+*/
+
+const cardIcons = [ "user-secret", "ambulance", "bath", "bell", "bug", "bomb", "birthday-cake", "tint", "volume-up",
+					"car", "camera", "camera-retro", "bus", "briefcase", "bowling-ball", "dna", "compass", "envelope",
+					"female", "eye", "fire-extinguisher", "first-aid", "filter", "fire", "flag", "football-ball",
+					"fly", "gem", "gamepad", "graduation-cap", "heartbeat", "headphones", "image", "shopping-bag",
+					"eye-dropper", "thermometer-half" ];
+
 document.addEventListener('DOMContentLoaded', function () {
 	document.querySelector("#main-menu ul").addEventListener("click", menuClickHandler);
 });
