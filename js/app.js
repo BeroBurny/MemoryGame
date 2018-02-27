8d172460d453663df6f24afd521a5feae371b3b8
WeedDominator <bero4net@gmail.com>
2018-02-27 18:06:12 +0100

    Cleaning card icon list

--- js/app.js
+++ js/app.js
@@ -5,18 +5,16 @@ user-secret		tint	ambulance		graduation-cap
 shopping-bag	bug 	bomb			bell
 birthday-cake	car		camera			fire-extinguisher
 camera-retro 	bus		briefcase		football-ball
-bowling-ball 	dna 	compass			thermometer-half
+bowling-ball 	gem	 	compass			thermometer-half
 envelope 		eye		female			heartbeat image
-eye-dropper		fly		filter			volume-up
-first-aid		gem		gamepad
+eye-dropper		filter	volume-up		gamepad
 fire flag		bath	headphones
 */
 
 const cardIcons = [ "user-secret", "ambulance", "bath", "bell", "bug", "bomb", "birthday-cake", "tint", "volume-up",
-					"car", "camera", "camera-retro", "bus", "briefcase", "bowling-ball", "dna", "compass", "envelope",
-					"female", "eye", "fire-extinguisher", "first-aid", "filter", "fire", "flag", "football-ball",
-					"fly", "gem", "gamepad", "graduation-cap", "heartbeat", "headphones", "image", "shopping-bag",
-					"eye-dropper", "thermometer-half" ];
+					"car", "camera", "camera-retro", "bus", "briefcase", "bowling-ball", "compass", "envelope",
+					"female", "eye", "fire-extinguisher", "filter", "fire", "flag", "football-ball", "gamepad",
+					"gem", "graduation-cap", "heartbeat", "headphones", "image", "shopping-bag", "eye-dropper", "thermometer-half" ];
 
 document.addEventListener('DOMContentLoaded', function () {
 	document.querySelector("#main-menu ul").addEventListener("click", menuClickHandler);
@@ -52,7 +50,6 @@ function  menuClickHandler(element){
 function buildGrid(){
 	for (var i = 0; i < 8; i++) {
 		let card = cardIcons[Math.floor(Math.random() * cardIcons.length)];
-		console.log(card);
 		createCards(card);
 		createCards(card);
 	}
